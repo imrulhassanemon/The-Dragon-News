2436054f23e68ed9a8a5d35a1272816ac2200cb4
imrulhassanemon <imrulhassan989@gmail.com>
2024-11-14 05:03:31 +0600

    The dragon news website

---
+++ src/Components/LatestNews.jsx
@@ -0,0 +1,11 @@
+
+
+const LatestNews = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LatestNews;
---
+++ src/layout/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header><Header></Header></header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;
